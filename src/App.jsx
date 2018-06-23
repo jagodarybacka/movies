89aee2da38a4838385dfc332e0040cfd5f0005ff
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2018-06-23 22:13:29 +0200

    Fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
-}
+const API = 'https://api.myjson.com/bins/1tll6';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: [],
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(API)
+      .then((response) => {
+        console.log(response.data)
+        this.setState({
+          fields: response.data.fields,
+          data: response.data.values
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
 export default App;
