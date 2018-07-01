5678d975f174c9d2cf38be515c1b07ab6cec5eda
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2018-07-01 12:54:55 +0200

    onChange select event

--- src/ControlsItem.jsx
+++ src/ControlsItem.jsx
@@ -1,40 +1,58 @@
 import React from 'react';
 import shordid from 'shortid';
 
-function ControlsItem({ name, defaultValue, options }) {
-  let optionsList = [];
-  if (options) {
-    optionsList = options.map((el) => {
-      let optionName;
-      let optionValue;
-      if (typeof el === 'string') {
-        optionName = el;
-        optionValue = el;
-      } else {
-        optionName = el.name;
-        optionValue = el.value;
-      }
-      return (
-        <option value={optionValue} key={shordid.generate()}>
-          {optionName}
-        </option>
-      );
-    });
+class ControlsItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 'none',
+    };
+    this.change = this.change.bind(this);
   }
 
-  return (
-    <li>
-      <p>
-        {name}
-      </p>
-      <select>
-        <option defaultValue>
-          {defaultValue || 'None'}
-        </option>
-        {optionsList}
-      </select>
-    </li>
-  );
+  change(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    const { options, name, defaultValue } = this.props;
+    let optionsList = [];
+    if (options) {
+      optionsList = options.map((el) => {
+        let optionName;
+        let optionValue;
+        if (typeof el === 'string') {
+          optionName = el;
+          optionValue = el;
+        } else {
+          optionName = el.name;
+          optionValue = el.value;
+        }
+        return (
+          <option value={optionValue} key={shordid.generate()}>
+            {optionName}
+          </option>
+        );
+      });
+    }
+    return (
+      <li>
+        <p>
+          {name}
+        </p>
+        <select
+          onChange={this.change}
+          value={this.state.value}
+        >
+          <option value="none" defaultValue>
+            {defaultValue || 'None'}
+          </option>
+          {optionsList}
+        </select>
+      </li>
+    );
+  }
 }
 
 export default ControlsItem;
